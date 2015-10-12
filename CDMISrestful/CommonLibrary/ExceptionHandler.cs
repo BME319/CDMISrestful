e16ec0181b84c2f287cc17ab443424dccad57722
WF <zjuwf2010@163.com>
2015-10-12 15:43:10 +0800

    wf logon register activation changpassword 20151012
    csq

--- CDMISrestful/CommonLibrary/ExceptionHandler.cs
+++ CDMISrestful/CommonLibrary/ExceptionHandler.cs
@@ -88,7 +88,144 @@ namespace CDMISrestful.CommonLibrary
             }
             return resp;
         }
-    
+
+        public HttpResponseMessage Register(int operationResult)
+        {
+
+            //
+            var resp = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            resp.Content = new StringContent(string.Format("注册失败"));
+            switch (operationResult)
+            {
+                case 1:
+                    //"已注册激活且有权限，登陆成功，跳转到主页";
+                    resp = new HttpResponseMessage(HttpStatusCode.OK);
+                    resp.Content = new StringContent(string.Format("注册成功，即将返回登录页面"));
+                    break;
+                case 2:
+                    //"已注册激活 但没有权限";
+                    resp = new HttpResponseMessage(HttpStatusCode.Forbidden);
+                    resp.Content = new StringContent(string.Format("注册失败"));
+                    break;
+                case 3:
+                    //您的账号对应的角色未激活，需要先激活；界面跳转到游客页面（已注册但未激活）
+                    resp = new HttpResponseMessage(HttpStatusCode.BadRequest);
+                    resp.Content = new StringContent(string.Format("用户名重复"));
+                    break;
+                case 4:
+                    //"用户不存在";
+                    resp = new HttpResponseMessage(HttpStatusCode.OK);
+                    resp.Content = new StringContent(string.Format("注册成功，密码与您的医生账号一致，即将返回登录页面"));
+                    break;
+                case 5:
+                    //"密码错误";
+                    resp = new HttpResponseMessage(HttpStatusCode.BadRequest);
+                    resp.Content = new StringContent(string.Format("用户名不能为空"));
+                    break;
+                case 6:
+                    //"密码错误";
+                    resp = new HttpResponseMessage(HttpStatusCode.BadRequest);
+                    resp.Content = new StringContent(string.Format("密码不能为空"));
+                    break;
+                case 7:
+                    //"密码错误";
+                    resp = new HttpResponseMessage(HttpStatusCode.BadRequest);
+                    resp.Content = new StringContent(string.Format("真实姓名不能为空"));
+                    break;
+                default:
+                    break;
+            }
+            return resp;
+        }
+
+        public HttpResponseMessage Activition(int operationResult)
+        {
+
+            //
+            var resp = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            resp.Content = new StringContent(string.Format("激活失败"));
+            switch (operationResult)
+            {
+                case 1:
+                    //"已注册激活且有权限，登陆成功，跳转到主页";
+                    resp = new HttpResponseMessage(HttpStatusCode.OK);
+                    resp.Content = new StringContent(string.Format("激活成功，首次使用请重置密码"));
+                    break;
+                case 2:
+                    //"已注册激活 但没有权限";
+                    resp = new HttpResponseMessage(HttpStatusCode.BadRequest);
+                    resp.Content = new StringContent(string.Format("邀请码错误，请确认后重新输入邀请码"));
+                    break;
+                case 3:
+                    //您的账号对应的角色未激活，需要先激活；界面跳转到游客页面（已注册但未激活）
+                    resp = new HttpResponseMessage(HttpStatusCode.BadRequest);
+                    resp.Content = new StringContent(string.Format("邀请码不能为空"));
+                    break;
+                default:
+                    break;
+            }
+            return resp;
+        }
+
+        public HttpResponseMessage ChangePassword(int operationResult)
+        {
+
+            //
+            var resp = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            resp.Content = new StringContent(string.Format("修改密码失败"));
+            switch (operationResult)
+            {
+                case 1:
+                    //"已注册激活且有权限，登陆成功，跳转到主页";
+                    resp = new HttpResponseMessage(HttpStatusCode.OK);
+                    resp.Content = new StringContent(string.Format("location.href = HomePage.html"));
+                    break;
+                case 2:
+                    //"已注册激活 但没有权限";
+                    resp = new HttpResponseMessage(HttpStatusCode.OK);
+                    resp.Content = new StringContent(string.Format("location.href = TaskMenu.html"));
+                    break;
+                case 3:
+                    //您的账号对应的角色未激活，需要先激活；界面跳转到游客页面（已注册但未激活）
+                    resp = new HttpResponseMessage(HttpStatusCode.BadRequest);
+                    resp.Content = new StringContent(string.Format("新密码设置失败，请联系管理员重置密码"));
+                    break;
+                case 4:
+                    //您的账号对应的角色未激活，需要先激活；界面跳转到游客页面（已注册但未激活）
+                    resp = new HttpResponseMessage(HttpStatusCode.BadRequest);
+                    resp.Content = new StringContent(string.Format("旧密码错误，请输入正确的旧密码"));
+                    break;
+                case 5:
+                    //您的账号对应的角色未激活，需要先激活；界面跳转到游客页面（已注册但未激活）
+                    resp = new HttpResponseMessage(HttpStatusCode.BadRequest);
+                    resp.Content = new StringContent(string.Format("密码已过期，请联系管理员重置密码"));
+                    break;
+                case 6:
+                    //您的账号对应的角色未激活，需要先激活；界面跳转到游客页面（已注册但未激活）
+                    resp = new HttpResponseMessage(HttpStatusCode.BadRequest);
+                    resp.Content = new StringContent(string.Format("旧密码不能为空"));
+                    break;
+                case 7:
+                    //您的账号对应的角色未激活，需要先激活；界面跳转到游客页面（已注册但未激活）
+                    resp = new HttpResponseMessage(HttpStatusCode.BadRequest);
+                    resp.Content = new StringContent(string.Format("新密码不能为空"));
+                    break;
+                case 8:
+                    //您的账号对应的角色未激活，需要先激活；界面跳转到游客页面（已注册但未激活）
+                    resp = new HttpResponseMessage(HttpStatusCode.BadRequest);
+                    resp.Content = new StringContent(string.Format("请再次输入新密码"));
+                    break;
+                case 9:
+                    //您的账号对应的角色未激活，需要先激活；界面跳转到游客页面（已注册但未激活）
+                    resp = new HttpResponseMessage(HttpStatusCode.BadRequest);
+                    resp.Content = new StringContent(string.Format("两次输入的密码不同，请再次确认新密码"));
+                    break;
+                default:
+                    break;
+            }
+            return resp;
+        }
+
     }
 }
                        
--- CDMISrestful/Controllers/UsersController.cs
+++ CDMISrestful/Controllers/UsersController.cs
@@ -16,11 +16,34 @@ namespace CDMISrestful.Controllers
 
         [Route("Api/v1/Users/LogOn")]
         [ModelValidationFilter]
-        [HttpGet]
         public HttpResponseMessage LogOn(LogOn logOn)
         {
             int ret = repository.LogOn(logOn.PwType, logOn.username, logOn.password, logOn.role);
             return new ExceptionHandler().LogOn(ret);
+        }
+
+        [Route("Api/v1/Users/Register")]
+        [ModelValidationFilter]
+        public HttpResponseMessage Register(Register Register)
+        {
+            int ret = repository.Register(Register.PwType, Register.userId, Register.UserName, Register.Password, Register.role, Register.revUserId, Register.TerminalName, Register.TerminalIP, Register.DeviceType);
+            return new ExceptionHandler().Register(ret);
+        }
+
+        [Route("Api/v1/Users/Activition")]
+        [ModelValidationFilter]
+        public HttpResponseMessage Activition(Activition Activition)
+        {
+            int ret = repository.Activition(Activition.UserId, Activition.InviteCode);
+            return new ExceptionHandler().Activition(ret);
+        }
+
+        [Route("Api/v1/Users/ChangePassword")]
+        [ModelValidationFilter]
+        public HttpResponseMessage ChangePassword(ChangePassword ChangePassword)
+        {
+            int ret = repository.ChangePassword(ChangePassword.OldPassword, ChangePassword.NewPassword, ChangePassword.ConfirmPassword, ChangePassword.UserId, ChangePassword.Device, ChangePassword.revUserId, ChangePassword.TerminalName, ChangePassword.TerminalIP, ChangePassword.DeviceType);
+            return new ExceptionHandler().ChangePassword(ret);
         }
     }
 }
